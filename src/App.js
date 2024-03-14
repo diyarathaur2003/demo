a8d6e811be6fc22f80ab7fcb7cf48a3152d77d0b
diyarathaur2003 <132491991+diyarathaur2003@users.noreply.github.com>
2024-03-14 10:11:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h3> Jenkins TASK - 6.2, DIYA RATHAUR, X1 </h3>
+     <h3> Jenkins TASK - 6.2, DIYA RATHAUR, X1 section </h3>
     </div>
   );
 }
